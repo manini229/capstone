8f45f7cf50934b63b52196bc941ea3a56dfb546d
Manini Choudhary <manini@Maninis-MacBook-Air.local>
2022-03-05 13:47:03 -0500

    Added Ultrasonic Sesnror File, Basic Movement Files

--- base_motor_control/base_motor_control.ino
+++ base_motor_control/base_motor_control.ino
@@ -16,7 +16,7 @@ void loop() {
    irrecv.resume(); // Receive the next value
  }
 }
-
+-----
 #include <boarddefs.h>
 #include <IRremote.h>
 #include <IRremoteInt.h>
@@ -77,3 +77,35 @@ void loop()
  irrecv.resume();                                   // receive the next value
  }  
 }
+
+ #define outputA 6
+ #define outputB 7
+
+ int counter = 0; 
+ int aState;
+ int aLastState;  
+
+ void setup() { 
+   pinMode (outputA,INPUT);
+   pinMode (outputB,INPUT);
+   
+   Serial.begin (9600);
+   // Reads the initial state of the outputA
+   aLastState = digitalRead(outputA);   
+ } 
+
+ void loop() { 
+   aState = digitalRead(outputA); // Reads the "current" state of the outputA
+   // If the previous and the current state of the outputA are different, that means a Pulse has occured
+   if (aState != aLastState){     
+     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+     if (digitalRead(outputB) != aState) { 
+       counter ++;
+     } else {
+       counter --;
+     }
+     Serial.print("Position: ");
+     Serial.println(counter);
+   } 
+   aLastState = aState; // Updates the previous state of the outputA with the current state
+ }
